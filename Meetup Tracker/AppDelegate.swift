f1f1abec4fd0583861ab5dff9209772a54fdd453
Anton Istomin <anton@tail-call.ru>
2019-02-14 13:53:47 +0700

    Initial Commit

---
+++ Meetup Tracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Meetup Tracker
+//
+//  Created by Anton Istomin on 2019-02-14.
+//  Copyright © 2019 Tail Call. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Meetup Tracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Meetup Tracker
+//
+//  Created by Anton Istomin on 2019-02-14.
+//  Copyright © 2019 Tail Call. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
